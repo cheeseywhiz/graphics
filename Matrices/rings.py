0be69c0f52604bbcf1b901359253b9c61c8bb773
Spencer Todd <cheesy.mac.dott@gmail.com>
2018-05-26 19:08:41 -0400

    Add frame vis to rings demo

--- Matrices/rings.py
+++ Matrices/rings.py
@@ -19,14 +19,14 @@ class OlympicRingsPlotter(Axes3D):
         self.index = 0
         self.slowness = slowness
 
-        self.ring = cg_objects.circle(cg_objects.Vector.i_hat * 10) \
+        self.ring = cg_objects.circle(cg_objects.Vector.i_hat * 2.5) \
             .rotate_y(math.pi / 2)
         self.base_frame = cg_objects.Frame.unit \
-            .translate(cg_objects.Vector(0, -24, 0))
+            .translate(cg_objects.Vector(0, -6, 0))
         self.lower = cg_objects.Frame.unit \
-            .translate(cg_objects.Vector(0, 12, -12))
+            .translate(cg_objects.Vector(0, 3, -3))
         self.upper = cg_objects.Frame.unit \
-            .translate(cg_objects.Vector(0, 12, 12))
+            .translate(cg_objects.Vector(0, 3, 3))
 
     def plot_vertices(self, vertices, **kwargs):
         xs, ys, zs, _ = np.array(vertices)
@@ -37,8 +37,18 @@ class OlympicRingsPlotter(Axes3D):
             .translate(cg_objects.Vector(-r, -r, -r))
         self.plot_vertices(cube, c='w', marker='.')
 
+    def plot_frame(self, frame, **kwargs):
+        vectors = frame.x, frame.y, frame.z
+        colors = kwargs.pop('colors', ((1, 0, 0), (0, 1, 0), (0, 0, 1)))
+
+        for vector, color in zip(vectors, colors):
+            super().quiver(
+                frame.origin.x, frame.origin.y, frame.origin.z,
+                vector.i, vector.j, vector.k,
+                colors=color, **kwargs)
+
     def redraw(self):
-        self.plot_minimums(40)
+        self.plot_minimums(10)
         rings = (
             (self.base_frame, 'b', 1),
             (self.lower, 'y', -1),
@@ -53,6 +63,7 @@ class OlympicRingsPlotter(Axes3D):
 
         for frame, color, direction in rings:
             ring_frame @= frame
+            self.plot_frame(ring_frame)
             self.plot_vertices(
                 ring_frame @ rotation[direction] @ self.ring, c=color
             )
@@ -67,7 +78,7 @@ class OlympicRingsPlotter(Axes3D):
 
 def main():
     fig = plt.figure()
-    ax = OlympicRingsPlotter(fig, 18)
+    ax = OlympicRingsPlotter(fig, 24)
     threading.Thread(target=loop, args=(ax.next, 6), daemon=True).start()
     plt.show()
 
