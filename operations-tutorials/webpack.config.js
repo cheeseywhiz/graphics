f2217e66e8bbae0332ca22e4b38c327fcfa2b923
Spencer Todd <cheesy.mac.dott@gmail.com>
2018-05-30 14:38:55 -0400

    Reconfigure in prep for multiple entry

--- operations-tutorials/webpack.config.js
+++ operations-tutorials/webpack.config.js
@@ -3,24 +3,24 @@ var HtmlWebPackPlugin = require('html-webpack-plugin');
 
 var ROOT_PATH = path.resolve(__dirname);
 var BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
-var APP_PATH = path.resolve(ROOT_PATH, 'app')
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var JS_PATH = path.resolve(APP_PATH, 'js');
-var ENTRY_PATH = path.resolve(JS_PATH, 'index.js');
-var TEMPLATE_PATH = path.resolve(APP_PATH, 'index.html');
-
-var debug = process.env.MODE_ENV !== 'production';
 
 module.exports = {
-    entry: ENTRY_PATH,
+    entry: {
+        index: path.resolve(JS_PATH, 'index.js'),
+    },
     output: {
         path: BUILD_PATH,
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
     },
     plugins: [
         new HtmlWebPackPlugin({
             title: 'Matrix Operations Tutorial',
-            template: TEMPLATE_PATH,
-            inject: 'body',
+            template: path.resolve(APP_PATH, 'index.html'),
+            filename: 'index.html',
+            chunks: ['index'],
+            inject: 'head',
         }),
     ],
     module: {
