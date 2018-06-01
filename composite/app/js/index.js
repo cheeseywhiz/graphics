588fc7ed3da577cce15ed0725ad961da0ea1f52a
Spencer Todd <cheesy.mac.dott@gmail.com>
2018-06-01 10:20:28 -0400

    Add operation matrix selector

--- composite/app/js/index.js
+++ composite/app/js/index.js
@@ -1,25 +1,152 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function addButton(id, tag, message, container) {
-    document.getElementById(id).addEventListener('click', function (ev) {
-        ReactDOM.render(
-            React.createElement(tag, null, message),
-            container
-        );
-    });
+class DefaultMatrix extends React.Component {
+    render() {
+        return <div>
+            <table class="matrix">
+                <tr>
+                    <td>1</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>0</td>
+                    <td>1</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>1</td>
+                </tr>
+            </table>
+        </div>
+    }
 }
 
+class ScaleMatrix extends React.Component {
+    render() {
+        return <div>
+            <table class="matrix">
+                <tr>
+                    <td><input type="number" id="input-xi" value="1"/></td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>0</td>
+                    <td><input type="number" id="input-yj" value="1"/></td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>1</td>
+                </tr>
+            </table>
+        </div>
+    }
+}
+
+class RotationMatrix extends React.Component {
+    render() {
+        return <div>
+            <input type="number" id="rotation-angle" placeholder="angle" />
+            <table class="matrix">
+                <tr>
+                    <td><input type="number" id="input-xi" value="1" disabled /></td>
+                    <td><input type="number" id="input-yi" value="0" disabled /></td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td><input type="number" id="input-xj" value="0" disabled /></td>
+                    <td><input type="number" id="input-yj" value="1" disabled /></td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>1</td>
+                </tr>
+            </table>
+        </div>
+    }
+}
+
+class TranslationMatrix extends React.Component {
+    render() {
+        return <div>
+            <table class="matrix">
+                <tr>
+                    <td>1</td>
+                    <td>0</td>
+                    <td><input type="number" id="input-ox" value="0" /></td>
+                </tr>
+                <tr>
+                    <td>0</td>
+                    <td>1</td>
+                    <td><input type="number" id="input-oy" value="0" /></td>
+                </tr>
+                <tr>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>1</td>
+                </tr>
+            </table>
+        </div>
+    }
+}
+
+class ManualMatrix extends React.Component {
+    render() {
+        return <div>
+            <table class="matrix">
+                <tr>
+                    <td><input type="number" id="input-xi" value="1" /></td>
+                    <td><input type="number" id="input-yi" value="0" /></td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td><input type="number" id="input-xj" value="0" /></td>
+                    <td><input type="number" id="input-yj" value="1" /></td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>1</td>
+                </tr>
+            </table>
+        </div>
+    }
+}
+
+function renderMatrix(selector, matrix) {
+    switch (selector.selectedIndex) {
+        case 1:
+            ReactDOM.render(<ScaleMatrix />, matrix);
+            break;
+        case 2:
+            ReactDOM.render(<RotationMatrix />, matrix);
+            break;
+        case 3:
+            ReactDOM.render(<TranslationMatrix />, matrix);
+            break;
+        case 4:
+            ReactDOM.render(<ManualMatrix />, matrix);
+            break;
+        default:
+            ReactDOM.render(<DefaultMatrix />, matrix);
+            break;
+    }
+}
 
 function main() {
-    const message = 'Hello World!';
-    const container = document.getElementById('container');
-    addButton('XL', 'h1', message, container);
-    addButton('L', 'h2', message, container);
-    addButton('M', 'h3', message, container);
-    addButton('S', 'h4', message, container);
-    addButton('XS', 'h5', message, container);
-    addButton('XXS', 'h6', message, container);
+    const selector = document.getElementById('operation');
+    const matrix = document.getElementById('input-matrix');
+    selector.addEventListener('change', (ev) => renderMatrix(selector, matrix));
+    renderMatrix(selector, matrix);
 }
 
 main();
