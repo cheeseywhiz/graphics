ee4c3161b5b58b1c453d7b0c0d4c9c2ba93384cf
Spencer Todd <cheesy.mac.dott@gmail.com>
2018-06-22 10:31:39 -0400

    Conditionally show intermediates

--- composite/src/index/App/Intermediates.js
+++ composite/src/index/App/Intermediates.js
@@ -3,18 +3,22 @@ import {connect, } from 'react-redux';
 import selectors from '../../selectors.js';
 import FrameList from './Stack/FrameList.js';
 
+const list = (label, frames) => <div>
+    <b>{label}</b>
+    <FrameList frames={frames} />
+</div>
+
 const mapStateToProps = (state) => ({
+    order: selectors.order(state),
     globals: selectors.globals(state),
     locals: selectors.locals(state),
 });
 
 const Intermediates = connect(mapStateToProps)(
-    ({globals, locals}) => (
+    ({order, globals, locals}) => (
         <div>
-            <b>Global frames</b>
-            <FrameList frames={globals} />
-            <b>Local frames</b>
-            <FrameList frames={locals} />
+            {order.globals && list('Global frames', globals)}
+            {order.locals && list('Local frames', locals)}
         </div>
     )
 );
