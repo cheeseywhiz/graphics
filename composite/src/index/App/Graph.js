523c64aefe312d512a1fa045870cfb33687d5381
Spencer Todd <cheesy.mac.dott@gmail.com>
2018-06-22 10:44:52 -0400

    Achieve graph updates without selector subscriptions

--- composite/src/index/App/Graph.js
+++ composite/src/index/App/Graph.js
@@ -1,18 +1,16 @@
 import {connect, } from 'react-redux';
-import actions from '../../actions.js';
 import selectors from '../../selectors.js';
 import BaseGraph from './Graph/BaseGraph.js';
 
-@connect()
-export default class Graph extends BaseGraph {
-    constructor(props) {
-        super(props);
-        this.props.dispatch(actions.selectorSubscribe(
-            selectors.globals, selectors.locals, selectors.order, selectors.shape,
-            this.reDrawScene.bind(this),
-        ));
-    }
+const mapStateToProps = (state) => ({
+    globals: selectors.globals(state),
+    locals: selectors.locals(state),
+    order: selectors.order(state),
+    shape: selectors.shape(state),
+});
 
+@connect(mapStateToProps)
+export default class Graph extends BaseGraph {
     addIntermediates(intermediates, color, shape) {
         intermediates
             .forEach((frame) => {
@@ -20,9 +18,10 @@ export default class Graph extends BaseGraph {
             });
     }
 
-    reDrawScene(globals, locals, order, shape) {
-        console.log('selector subscription');
-        console.table({globals, locals});
+    render() {
+        console.log('rerendering Graph');
+        const {globals, locals, order, shape} = this.props;
+
         const first = globals.slice(0, 1);
         const last = globals.slice(-1);
         this.scene.clear();
@@ -30,5 +29,7 @@ export default class Graph extends BaseGraph {
         if (order.locals) this.addIntermediates(locals.slice(1, -1), 0x0000ff, shape);
         if (order.globals) this.addIntermediates(globals.slice(1, -1), 0xff0000, shape);
         if (globals.length > 1) this.addIntermediates(last, 0xffffff, shape);
+
+        return super.render();
     }
 }
