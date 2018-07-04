2573ff659c18c271d783f7193a26efb720b70e34
Spencer Todd <cheesy.mac.dott@gmail.com>
2018-07-04 00:46:19 -0400

    Use flex layout for input matrix

--- composite/src/App/Stack/InputMatrix/common/InputMatrixBase/InputMatrixBase.js
+++ composite/src/App/Stack/InputMatrix/common/InputMatrixBase/InputMatrixBase.js
@@ -5,16 +5,16 @@ import ResetButton from './ResetButton.js';
 import style from './InputMatrixBase.css';
 
 export default ({matrix, input}) => <div className={style.inputMatrix}>
-    <div className={style.selector}>
-        <OperationSelector />
-    </div>
-    <div className={style.reset}>
+    <div className={style.topRow}>
+        <div className={style.selector}>
+            <OperationSelector />
+        </div>
         <ResetButton />
     </div>
-    {input && <div className={style.input}>
-        {input}
-    </div>}
-    <div className={style.matrixContainer}>
-        <Matrix matrix={matrix} />
-    </div>
+    {input &&
+        <div className={style.input}>
+            {input}
+        </div>
+    }
+    <Matrix matrix={matrix} />
 </div>
