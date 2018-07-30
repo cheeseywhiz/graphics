864a39d2db657d79ac4fb0d9a6c85f7f180e2b33
Spencer Todd <cheesy.mac.dott@gmail.com>
2018-07-29 20:25:35 -0400

    Move composite folder to own repo

--- composite/src/App/common/selectors/common/baseSelectors.js
+++
@@ -1,8 +0,0 @@
-export default {
-    matrix: (state) => state.matrix,
-    operation: (state) => state.operation,
-    geometry: (state) => state.geometry,
-    entryOrder: (state) => state.entryOrder,
-    shape: (state) => state.shape,
-    stack: (state) => state.stack,
-};
